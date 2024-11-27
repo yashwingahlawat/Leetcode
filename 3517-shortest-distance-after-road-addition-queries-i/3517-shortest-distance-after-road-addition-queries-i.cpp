9a931199441980bd1956eedd517265dc66bf4373
yashwingahlawat <110126076+yashwingahlawat@users.noreply.github.com>
2024-11-27 17:32:27 +0530

    Time: 58 ms (82.38%), Space: 109 MB (68.62%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -5,35 +5,25 @@ public:
         for(int i=0;i<n-1;i++){
             adj[i].push_back(i+1);
         }
-        
-        // for(auto i:queries){
-        //     adj[i[0]].push_back(i[1]);
-        // }
-        // queue<int>q;
-        vector<int>dist(n,0);
         vector<int>res;
-        for(int i=0;i<dist.size();i++)dist[i]=i;
+        vector<int>dist(n,0);
+        for(int i=0;i<n;i++)dist[i]=i;
         for(auto i:queries){
+            queue<int>q;
             adj[i[0]].push_back(i[1]);
-            if(dist[i[0]]+1<dist[i[1]]){
-                queue<int>q;
-                dist[i[1]]=dist[i[0]]+1;
-                q.push(i[1]);
-                while(!q.empty()){
-                    int x=q.front();
-                    q.pop();
-                    for(auto y:adj[x]){
-                        if(dist[x]+1<dist[y]){
-                            dist[y]=dist[x]+1;
-                            q.push(y);
-                        }
+            q.push(i[0]);
+            while(!q.empty()){
+                int x=q.front();
+                q.pop();
+                for(auto i:adj[x]){
+                    if(dist[x]+1<dist[i]){
+                        dist[i]=dist[x]+1;
+                        q.push(i);
                     }
                 }
             }
             res.push_back(dist[n-1]);
         }
         return res;
-        // dist[0]=0;
-        // d
     }
 };
